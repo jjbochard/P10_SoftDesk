619319fa92408505c5889fb2213e247064856d7e
jeremy.bochard <jeremy.bochard@annalect.com>
2022-02-23 10:15:39 +0100

    update models and make migrations

--- softdesk/app/models.py
+++ softdesk/app/models.py
@@ -1,36 +1,35 @@
 from django.conf import settings
 from django.db import models
-from django.forms import CharField
 
 PERMISSION_CHOICES = ()
 
 
 class Project(models.Model):
-    title = CharField(max_length=255)
-    description = CharField(max_length=255)
-    type = CharField(max_length=255)
+    title = models.CharField(max_length=255)
+    description = models.CharField(max_length=255)
+    type = models.CharField(max_length=255)
     user = models.ManyToManyField(to=settings.AUTH_USER_MODEL, through="Contributor")
 
 
 class Contributor(models.Model):
-    role = CharField(max_length=100)
+    role = models.CharField(max_length=100)
     user = models.ForeignKey(to=settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     project = models.ForeignKey(Project, on_delete=models.CASCADE)
 
 
 class Issue(models.Model):
-    title = CharField(max_length=255, primary_key=True)
-    description = CharField(max_length=255)
-    tag = CharField(max_length=50)
-    priority = CharField(max_length=50)
-    status = CharField(max_length=50)
+    title = models.CharField(max_length=255, primary_key=True)
+    description = models.CharField(max_length=255)
+    tag = models.CharField(max_length=50)
+    priority = models.CharField(max_length=50)
+    status = models.CharField(max_length=50)
     project = models.ForeignKey(Project, on_delete=models.CASCADE)
     user = models.ForeignKey(to=settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     created_time = models.DateTimeField(auto_now_add=True)
 
 
 class Comment(models.Model):
-    description = CharField(max_length=255)
+    description = models.CharField(max_length=255)
     user = models.ForeignKey(to=settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     issue = models.ForeignKey(Issue, on_delete=models.CASCADE)
     created_time = models.DateTimeField(auto_now_add=True)
