f9b3c3f8e77067b0a9e6db5d06f8e20b3109195c
Jérémy Bochard <jeremy.bochard@gmail.com>
2022-02-26 18:10:36 +0100

    add ModelAdmin for apps

--- softdesk/app/admin.py
+++ softdesk/app/admin.py
@@ -1,3 +1,45 @@
-# from django.contrib import admin
+from app.models import Comment, Contributor, Issue, Project
+from django.contrib import admin
 
-# Register your models here.
+
+class ProjectAdmin(admin.ModelAdmin):
+    list_display = (
+        "title",
+        "description",
+        "type",
+    )
+
+
+class ContributorAdmin(admin.ModelAdmin):
+    list_display = (
+        "project",
+        "user",
+        "role",
+    )
+
+
+class IssueAdmin(admin.ModelAdmin):
+    list_display = (
+        "title",
+        "description",
+        "tag",
+        "priority",
+        "status",
+        "project",
+        "author",
+        "assignee",
+    )
+
+
+class CommentAdmin(admin.ModelAdmin):
+    list_display = (
+        "description",
+        "author",
+        "issue",
+    )
+
+
+admin.site.register(Project, ProjectAdmin)
+admin.site.register(Contributor, ContributorAdmin)
+admin.site.register(Issue, IssueAdmin)
+admin.site.register(Comment, CommentAdmin)
